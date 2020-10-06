a9b7095375248f31a18f02bdc10c1417a2c87264
Lucas Tayar <lucastayar12@gmail.com>
2020-10-05 21:48:46 -0300

    FIM DA TAREFA NODE E EXPRESS

--- Aula_node.js/main.js
+++ Aula_node.js/main.js
@@ -2,8 +2,21 @@ const express = require('express');
 const app = express();
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+  res.send("Seja Bem vindo <button><a src='./views/sobre.html'>Sobre</a></button>");
 });
+
+app.get("/sobre", (req, res) => {
+  res.sendFile(__dirname + "/views/sobre.html");
+});
+
+app.get("/contato", (req, res) => {
+  res.sendFile(__dirname + "/views/contato.html");
+});
+
+app.get("/confirmacao", (req, res) => {
+  res.sendFile(__dirname + "/views/confirmacao.html");
+});
+
 
 app.listen(3000, () => {
   console.log("Servidor ligado");
