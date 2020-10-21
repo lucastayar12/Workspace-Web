272bae3a6054a5cc7253b83553c0fe5bd5ac184a
Lucas Tayar <lucastayar12@gmail.com>
2020-10-21 20:38:20 -0300

    BANCO CHANGES PACKAGE

--- Aula_modules.js/banco.js
+++ Aula_modules.js/banco.js
@@ -4,8 +4,32 @@ const sequelize = new Sequelize('Alunos', 'postgres', '123456', {
     dialect: 'postgres'
 })
 
-sequelize.authenticate().then(() => {
+/*sequelize.authenticate().then(() => {
     console.log("Conexão sucesso")
 }).catch((err) => {
     console.log("Erro " + err)
+})*/
+
+const Alunos = sequelize.define('Alunos', {
+    nome: {
+        type: Sequelize.STRING
+    },
+    idade: {
+        type: Sequelize.INTEGER
+    }
+});
+
+Alunos.sync({ force: true }).then(() => {
+    console.log("Tabela Criada");
+
+    return Alunos.create({
+        nome: 'Lucas Tayar',
+        idade: 19
+    })
+}).then(() => {
+    return Alunos.findAll();
+}).then((data) => {
+    console.log(data);
+}).catch((err) => {
+    console.log(err);
 })
